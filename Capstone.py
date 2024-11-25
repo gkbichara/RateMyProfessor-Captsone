819b60f5482c2a7c863213f292e4f95ab4c0c51d
Galal Bichara <40720218+gkbichara@users.noreply.github.com>
2024-11-25 15:56:16 -0500

    Update Capstone.py
    Refined q1 by adding figures about male and female ratings
    
    Did question 3 and changed the threshold for it

--- Capstone.py
+++ Capstone.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Nov 19 13:42:07 2024
 
-@author: Galal BIchara
+@author: Galal Bichara
 """
 # %% Loading in all the libraries and datasets
 import numpy as np
@@ -20,10 +20,10 @@ numericalDf = pd.read_csv("/Users/gkb/Desktop/PODS/Capstone/rmpCapstoneNum.csv",
 qualitativeDf = pd.read_csv("/Users/gkb/Desktop/PODS/Capstone/rmpCapstoneQual.csv", header = None, 
             names = ["Major/Field", "University", "State"])
 
-noNanAvgRatings = numericalDf.dropna(subset=['AvgRating']).shape[0]
+noNanNumericalDf = numericalDf.dropna(subset=['AvgRating'])
 
 
-threshold = 5
+threshold = noNanNumericalDf['numRatings'].quantile(0.5)
 
 
 
@@ -33,7 +33,7 @@ threshold = 5
 # EDA
 avgRatingBins = np.arange(0, 5.5, 0.5)  # Creating bins from 0 to 5 with a step of 0.5
 plt.figure(figsize=(10, 6))
-plt.hist(numericalDf['AvgRating'], bins=avgRatingBins, edgecolor='black', alpha=0.7)
+plt.hist(noNanNumericalDf['AvgRating'], bins=avgRatingBins, edgecolor='black', alpha=0.7)
 plt.title('Distribution of Average Ratings')
 plt.xlabel('Average Rating')
 plt.ylabel('Frequency')
@@ -45,8 +45,8 @@ plt.show()
 plt.figure(figsize=(10, 6))
 # Use logarithmic bins for better visual representation of skewed data
 ratingsBins = np.concatenate([np.arange(1, 51, 2), np.arange(51, 401, 25)])  # Finer bins in the lower range
-plt.hist(numericalDf['numRatings'], bins=ratingsBins, edgecolor='black', alpha=0.7)
-plt.title('Distribution of Number of Ratings')
+plt.hist(noNanNumericalDf['numRatings'], bins=ratingsBins, edgecolor='black', alpha=0.7)
+plt.title('Distribution of Average Ratings')
 plt.xlabel('Number of Ratings')
 plt.ylabel('Frequency')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
@@ -55,14 +55,36 @@ plt.show()
 
 
 
-
-
-
 # Filter dataset to only include professors with >= threshold number of ratings
-filteredDf = numericalDf[numericalDf['numRatings'] >= threshold]
+q1Df = noNanNumericalDf[noNanNumericalDf['numRatings'] > threshold]
+
+femaleProfs = q1Df[q1Df["Female"] == 1]
+maleProfs = q1Df[q1Df["Male"] == 1]
+maleProfRatings = maleProfs["AvgRating"]
+femaleProfRatings = femaleProfs["AvgRating"]
+
+q1u, q1p = stats.mannwhitneyu(maleProfRatings, femaleProfRatings)
 
 
 
+avgRatingBins = np.arange(0, 5.5, 0.5)  # Creating bins from 0 to 5 with a step of 0.5
+plt.figure(figsize=(10, 6))
+plt.hist(femaleProfs['AvgRating'], bins=avgRatingBins, edgecolor='black', alpha=0.7)
+plt.title('Distribution of Female Ratings')
+plt.xlabel('Average Rating')
+plt.ylabel('Frequency')
+plt.xticks(avgRatingBins)  # Setting x-ticks to match the bin edges for clarity
+plt.show()
+
+
+avgRatingBins = np.arange(0, 5.5, 0.5)  # Creating bins from 0 to 5 with a step of 0.5
+plt.figure(figsize=(10, 6))
+plt.hist(maleProfs['AvgRating'], bins=avgRatingBins, edgecolor='black', alpha=0.7)
+plt.title('Distribution of Male Ratings')
+plt.xlabel('Average Rating')
+plt.ylabel('Frequency')
+plt.xticks(avgRatingBins)  # Setting x-ticks to match the bin edges for clarity
+plt.show()
 
 
 # %% Question 2
@@ -75,23 +97,41 @@ filteredDf = numericalDf[numericalDf['numRatings'] >= threshold]
 
 # %% Question 3
 
-avgRatingArray = np.array([filteredDf["AvgRating"]])
-avgDifficultyArray = np.array([filteredDf["AvgDifficulty"]])
+from scipy.stats import spearmanr
 
-question3nans = np.array([np.isnan(avgRatingArray), np.isnan(avgDifficultyArray)], dtype = bool)
 
-# same info but in numbers
-question3nans2 = question3nans * 1
+q3df = noNanNumericalDf[noNanNumericalDf['numRatings'] > 15]
 
-# summing tells you how many movies each person is missing (0 means they rated all 3)
-question3nans3 = sum(question3nans2)
 
-missingData = np.where(question3nans3 > 0)
+avgRatingArray = q3df["AvgRating"].values  
+avgDifficultyArray = q3df["AvgDifficulty"].values  
 
-# delete all users that have not rated all 3 movies
-# make sure you delete same users from all 3
-avgRatingArray = np.delete(avgRatingArray, missingData)
-avgDifficultyArray = np.delete(avgDifficultyArray, missingData)
 
-plt.scatter(avgRatingArray, avgDifficultyArray)
+valid_mask = ~np.isnan(avgRatingArray) & ~np.isnan(avgDifficultyArray)
 
+
+avgRatingArray_clean = avgRatingArray[valid_mask]
+avgDifficultyArray_clean = avgDifficultyArray[valid_mask]
+
+
+plt.figure(figsize=(10, 6))
+plt.scatter(avgRatingArray_clean, avgDifficultyArray_clean, alpha=0.7)
+plt.xlabel('Average Rating')
+plt.ylabel('Average Difficulty')
+plt.title('Average Rating vs Average Difficulty (Cleaned Data)')
+plt.grid(axis='both', linestyle='--', alpha=0.7)
+plt.show()
+
+rho = spearmanr(avgRatingArray_clean, avgDifficultyArray_clean)
+
+
+
+
+
+
+# %% Question 5
+
+
+noNanPropTakeAgainDf = numericalDf.dropna(subset=['propTakeAgain'])
+nonNanPropWithThreshold = noNanPropTakeAgainDf[noNanPropTakeAgainDf["numRatings"] > 3]
+# can do without imputation, have 12k ratings, Circle back to it
